19e6b3fdb4f56d3ec44d68e4fae7a549e88eb511
Rohit <rohitsingh@vmartretail.com>
2023-06-18 23:01:51 +0530

    remove the reducer

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import LoginPage from "./Pages/LoginPage";
 import { Route, Routes } from "react-router-dom";
 import AuthProtectedRoutes from "./Routes/AuthProtected";
 import TaskListPage from "./Pages/TaskListPage";
-import { useSelector } from "react-redux";
 
 function App() {
-  // const all = useSelector((state) => state);
-
-  // console.log("all:::", all);
-
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
